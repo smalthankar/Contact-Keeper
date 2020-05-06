4e6e05f342a589fd9c4516959aa9184da2761513
Suyash Malthankar <suyashmalthankar.743@gmail.com>
2020-05-06 19:19:24 -0400

    Front End setup, 2 grid - Contact Form and List

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,76 @@
+import React, { useReducer } from "react";
+import { v4 as uuid } from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from "../types";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "David Warner",
+        email: "warney@gmail.com",
+        phone: "111-111-1111",
+        type: "personal"
+      },
+      {
+        id: 2,
+        name: "Aaron Finch",
+        email: "finchy@gmail.com",
+        phone: "222-222-2222",
+        type: "professional"
+      },
+      {
+        id: 3,
+        name: "Steven Smith",
+        email: "stevey@gmail.com",
+        phone: "333-333-3333",
+        type: "professional"
+      },
+      {
+        id: 4,
+        name: "Marnus Labuschagne",
+        email: "marney@gmail.com",
+        phone: "444-444-4444",
+        type: "personal"
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+  const addContact = contact => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
